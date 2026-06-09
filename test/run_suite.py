ea9da0c35bfa7259a49edd37a9ce713d53b3b410
chenxiaobing <chenxiaobing@cambricon.com>
2026-06-09 10:57:15 +0800

    [MLU][8/N] Add MLU CI workflow and cambricon runner

--- test/run_suite.py
+++ test/run_suite.py
@@ -124,7 +124,8 @@ PER_COMMIT_SUITES = {
         "stage-b-e2e-mlx",
     ],
     HWBackend.MLU: [
-        "pr-test-mlu",
+        "pr-test-1-mlu",
+        "pr-test-2-mlu",
     ],
 }
 
@@ -201,7 +202,7 @@ NIGHTLY_SUITES = {
         "nightly-xpu-8-gpu",
     ],
     HWBackend.MLU: [
-        "nightly-test-mlu",
+        "nightly-test-2-mlu",
     ],
 }
 
